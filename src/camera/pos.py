6e2459b076d6746ba07d741e9544c064ce91d94a
DarkcrusherX <ahalmartinv@gmail.com>
2020-07-25 15:46:12 +0530

    pos oscillating

--- src/camera/pos.py
+++ src/camera/pos.py
@@ -8,17 +8,27 @@ from geometry_msgs.msg import TwistStamped
 from armf import armtakeoff
 
 rospy.init_node('navigation',anonymous=True)
+i=0
 
 def main() :
     while True:
         local_pos_pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size=10)
 
         pose = PoseStamped()
-        pose.pose.position.x = 0
-        pose.pose.position.y = 1
-        pose.pose.position.z = 3.27
+        pose.pose.position.x = 0.7
 
-        local_pos_pub.publish(pose)  
+        global i
+        t0=rospy.Time.now().to_sec()                                                   # To just wander left and ri8 to get a detection
+        while rospy.Time.now().to_sec()-t0 < 2:                                        # its enough i think 
+            if i%2 == 0:
+                pose.pose.position.y = 1
+            else:
+                pose.pose.position.y = -1
+
+            pose.pose.position.z = 3.27
+            pose.pose.orientation.w =1
+            local_pos_pub.publish(pose) 
+        i = i+1
 
 
 if __name__ == '__main__':
